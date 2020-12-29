6f8ef8d4f7e190a37cb9fe6fab62682425b5e302
tsukudamayo <tsukudamayo@gmail.com>
2020-12-29 03:55:02 +0000

    add scala-tdd project

---
+++ scala-tdd/helloworld/build.sbt
@@ -0,0 +1,8 @@
+name := "HelloWorld"
+version := "1.0"
+scalaVersion := "2.13.4"
+
+libraryDependencies +=
+"org.scalatest" %% "scalatest" % "3.0.8" % Test
+
+
---
+++ scala-tdd/helloworld/src/main/scala/simpletest/Hello.scala
@@ -0,0 +1,8 @@
+package simpletest
+
+object Hello extends App {
+  val p = new Person("Alvin Alexander")
+  println(s"Hello ${p.name}")
+}
+
+class Person(var name: String)
