fa3f528e19ef0c120d718569d8936d99013576c8
tsukudamayo <tsukudamayo@gmail.com>
2021-01-27 08:38:58 +0000

    bugfix target xpath css-selector

--- puppeteer-crawler/build.sh
+++ puppeteer-crawler/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+xhost +local:
 docker run \
     -it --rm \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $(dirname $(pwd);):/workspace/app \
     -v /media/tsukudamayo/0CE698DCE698C6FC2/tmp/data:/workspace/data \
     -e DISPLAY=$DISPLAY \
-    ts-dev \
+    puppeteer-ts-dev \
     /bin/bash
+xhost -local:
