0154e87fa0f51d9a10efef62d2027daa94bf4219
tsukudamayo <tsukudamayo@gmail.com>
2021-07-03 03:55:35 +0000

    practical-aws-cdk init

---
+++ practical-aws-cdk/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2019,
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  rules: {
+    "@typescript-eslint/interface-name-prefix": [
+      "error", {
+        "prefixWithI": "always"
+      }
+    ],
+    "@typescript-eslint/no-floating-promises": [
+      "error"
+    ],
+    "@typescript-eslint/no-unused-vars": [
+      "error", {
+        "argsIgnorePattern": "^_"
+      }
+    ],
+  }
+};
