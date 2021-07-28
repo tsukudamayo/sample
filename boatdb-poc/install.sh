e179ed54206cab04b9ad70393b8a6af96a2ee66a
tsukudamayo <tsukudamayo@gmail.com>
2021-07-28 05:20:08 +0000

    cloud sql setting

---
+++ boatdb-poc/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+# apt-get install -y apt-transport-https ca-certificates gnupg
+# curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+# apt-get update && apt-get install -y google-cloud-sdk
+# gcloud init --console-only
+# gcloud auth login
+# gcloud auth application-default login
+# 
+# wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
+# chmod +x cloud_sql_proxy
+
+apt-get update && apt-get install -y mariadb-server-10.3
+
