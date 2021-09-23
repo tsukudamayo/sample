eec375ba23f9b2daadfe948904ee5ac5f0efcf4d
tsukudamayo <tsukudamayo@gmail.com>
2021-09-23 05:47:34 +0000

    fix gcloud login

--- boatdb-poc/install.sh
+++ boatdb-poc/install.sh
@@ -4,7 +4,6 @@ echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.clou
 apt-get install -y apt-transport-https ca-certificates gnupg
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 apt-get update && apt-get install -y google-cloud-sdk
-gcloud init --console-only
 gcloud auth login
 gcloud auth application-default login
 
