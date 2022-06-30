598f2707b8832dbbc7eebf15a718b55f896ac18d
tsukudamayo <tsukudamayo@gmail.com>
2022-06-30 13:30:27 +0900

    add AtCoder Beginners Selection

--- atcoder/rust/atcoder-biginners-selection/src/main.rs
+++ atcoder/rust/atcoder-biginners-selection/src/main.rs
@@ -14,8 +14,50 @@ fn read<T: FromStr>() -> T {
 }
 
 fn main() {
+    // PracticeA - Welcome to AtCoder
+    let a: u32 = read();
+    let b: u32 = read();
+    let c: u32 = read();
+    let s: String = read();
+    let sum: u32 = a + b + c;
+    let sum_string: String = sum.to_string();
+    println!("{} {}", sum_string, s);
+
+    // ABC086A - Product
     let a: u32 = read();
     let b: u32 = read();
     let ans = if (a * b) % 2 == 0 { "Even" } else { "Odd" };
     println!("{}", ans);
+
+    // ABC081A - Placing Marbles
+    let mut count: i32 = 0;
+    let inputs: String = read();
+    let inputs_iter: Vec<char> = inputs.chars().collect();
+    for i in inputs_iter.iter() {
+	let tmp_string = i.to_string();
+	if tmp_string == "1" {
+	    count = count + 1;
+	}
+    }
+    println!("{}", count);
+
+    // // ABC081B - Shift only
+    // let mut count: u32 = 0;
+    // let num_of_inputs: u32 = read();
+    // let inputs: String = read();
+    // let inputs_iter: Vec<char> = inputs.chars().collect();
+    // for i in inputs_iter.iter() {
+    // 	let tmp_string: String = i.to_string();
+    // 	let tmp_number: i32 = tmp_string.parse().unwrap();
+    // 	if tmp_number % 2 != 0 {
+    // 	    break
+    // 	}
+    // 	count = count + 1;
+    // }
+    // println!("{}", count);
 }
+
+
+
+
+
--- atcoder/rust/build-multipass.sh
+++ atcoder/rust/build-multipass.sh
@@ -2,16 +2,17 @@
 
 export DOCKER_BUILDKIT=1
 
-docker build -t rust-dev -f dev.Dockerfile .
+docker build -t atcoder-rust -f dev.Dockerfile .
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     xhost +$(multipass list | grep docker-vm | awk '{print $3}')
     docker run -it --rm \
+        -v $(pwd):/workspace \
         -v /tmp:/tmp \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
         -e DISPLAY=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}'):0.0 \
-        --name rust-dev \
-        rust-dev \
+        --name atcoder-rust \
+        atcoder-rust \
         /bin/bash
     xhost -$(multipass list | grep docker-vm | awk '{print $3}')
 else
@@ -19,8 +20,8 @@ else
     docker run -it --rm \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
         -e DISPLAY=$DISPLAY \
-        --name rust-dev \
-        rust-dev \
+        --name atcoder-rust \
+        atcoder-rust \
         /bin/bash
     xhost -local:
 fi
